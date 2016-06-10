ebd15143e1844c1bc531b8bcbf6d0c85e34cc2c1
Tim Riley <tim@openmonkey.com>
2016-06-10 16:31:01 +1000

    Add an index file

---
+++ index.rb
@@ -0,0 +1,15 @@
+# # A conversational introduction to rom-rb
+#
+# An introduction to [rom-rb][rom-rb] in literate programming style. Learn rom-rb while seeing code and conversation side-by-side.
+#
+# - [**Part 1**](/part-1.html): Setup, relations, repositories and create commands
+# - [**Part 2**](/part-2.html): Types, associations, and update commands
+#
+# _Written by [Tim Riley][tw]._
+#
+# _[View the source][gh]_ · _[Follow the blog][blog]_ · _[Say hi on Twitter][tw]_
+#
+# [rom-rb]: http://rom-rb.org/
+# [gh]: https://github.com/icelab/conversational-intro-to-rom-rb
+# [blog]: http://icelab.com.au/articles/a-conversational-introduction-to-rom-rb/
+# [tw]: http://twitter.com/timriley
